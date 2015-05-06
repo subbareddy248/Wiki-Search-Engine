a5e0cc347961a2db5adfc4b1717650082cf9e1a4
subbareddy248 <subbareddyoota@gmail.com>
2015-05-06 13:34:36 +0530

    Create WikipediaBean.java

---
+++ src/wikiphase2/WikipediaBean.java
@@ -0,0 +1,76 @@
+package wikiphase2;
+
+/**
+ * Common tags
+ * @author subba
+ *
+ */
+public class WikipediaBean {
+
+	public final static String PAGE = "page";
+
+	public final static String FILE = "file";
+
+	
+	public final static String TITLE = "title";
+
+	public final static String TEXT = "text";
+	
+	public final static String REVISION = "revision";
+	
+	public final static String TIMESTAMP = "timestamp";
+
+	public final static String CONTRIBUTOR = "contributor";
+	
+	public final static String COMMENT = "comment";
+	
+	public final static String ID = "id";
+	
+	public final static String INFOBOX = "infobox";
+	
+	public final static String MEDIAWIKI = "mediawiki";
+	
+	public final static char hypen = '-';
+	
+	public final static char sep = '_';
+
+	public final static char colon = ':';
+
+	public final static char newline = '\n';
+	
+	public final static char equal = '=';
+
+	public final static char comma = ',';
+	public final static char A = 'a';
+	public final static char B = 'b';
+	public final static char C = 'c';
+	public final static char D = 'd';
+	public final static char E = 'e';
+	public final static char F = 'f';
+	public final static char G = 'g';
+	public final static char H = 'h';
+	public final static char I = 'i';
+	public final static char J = 'j';
+	public final static char K = 'k';
+	public final static char L = 'l';
+	public final static char M = 'm';
+	public final static char N = 'n';
+	public final static char O = 'o';
+	public final static char P = 'p';
+	public final static char Q = 'q';
+	public final static char R = 'r';
+	public final static char S = 's';
+	public final static char T = 't';
+	public final static char U = 'u';
+	public final static char V = 'v';
+	public final static char W = 'w';
+	public final static char X = 'x';
+	public final static char Y = 'y';
+	public final static char Z = 'z';
+	public final static String equalstring = "=";
+	public final static String ZERO = "0";
+	public final static String ONE = "1";
+	public final static String colonString = ":";
+	//public final static String 
+
+}
